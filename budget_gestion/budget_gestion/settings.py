9d212282b6669cd24381e049d38daa890d885462
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-12-15 13:01:46 +0000

    recuperer le travail de Moussa

--- budget_gestion/budget_gestion/settings.py
+++ budget_gestion/budget_gestion/settings.py
@@ -140,6 +140,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
+
 
 # REST Framework settings
 REST_FRAMEWORK = {
