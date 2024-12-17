a98dd6af3353db9fc567f8e48911d8ed6efd992e
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-12-17 15:27:54 +0000

    commenter

--- budget_gestion/budget_gestion/settings.py
+++ budget_gestion/budget_gestion/settings.py
@@ -75,15 +75,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'budget_gestion.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 # DATABASES = {
 #     'default' : {
