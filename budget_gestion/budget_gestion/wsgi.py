299aeb852e3d0e0f2cefedae8a8ba774557693d7
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-12-18 09:59:30 +0000

    ajouter

--- budget_gestion/budget_gestion/wsgi.py
+++ budget_gestion/budget_gestion/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'test_budget.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'budget_gestion.settings')
 
 application = get_wsgi_application()
