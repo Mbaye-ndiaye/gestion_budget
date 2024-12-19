3765e1fddaedab5d2a4f4d9cd9bd9fde587a0540
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-12-19 09:11:44 +0000

    tetse

--- gestionapp/apps.py
+++ gestionapp/apps.py
@@ -1,10 +1,10 @@
-
 from django.apps import AppConfig
+
 
 class GestionappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'gestionapp'
 
     def ready(self):
-        import gestionapp.signals  # Import des signaux ici
+        import gestionapp.signals
 
