d951052b203cc04afd576e16d262ad8be5794de2
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-12-19 10:31:13 +0000

    tetsrer

--- budget_gestion/settings.py
+++ budget_gestion/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = env('SECRET_KEY')
 # DEBUG = True
 DEBUG = config('DEBUG', default=False,cast=bool)
 
-# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["gestion-budget-rwdz.onrender.com", "http://127.0.0.1:8000/"]
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
+# ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
 
 # Application definition
