734a89c673a0872043525ad5ba808aaf340ee40f
Mbaye Ndiaye <babacarndiay546@gmail.com>
2024-12-19 12:36:16 +0000

    ajouter le lien deploiement des le ALLOWED_HOST et le lien

--- budget_gestion/settings.py
+++ budget_gestion/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from pathlib import Path
 BASE_DIR = Path(__file__).resolve().parent.parent
+import os
 import environ 
 env = environ.Env()
 environ.Env.read_env(env_file=str(BASE_DIR / ".env"))
@@ -25,7 +26,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = False
 # DEBUG = config('DEBUG', default=env(False),cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["gestion-budget-rwdz.onrender.com", "http://0.0.0.0:10000"]
 
 # ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 
@@ -142,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
