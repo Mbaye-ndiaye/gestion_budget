849ed14103e404a9cb8d181c96439c9ec8396c3d
Mbaye Ndiaye <babacarndiay546@gmail.com>
2025-01-07 16:14:26 +0000

    la deconnexion

--- gestionapp/urls.py
+++ gestionapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import (RegistrationView, LoginView, AddTransactionView, ListTransactionsView, BudgetView, BudgetInitialView)
+from .views import (RegistrationView, LoginView, LogoutView, AddTransactionView, ListTransactionsView, BudgetView, BudgetInitialView)
 
 urlpatterns = [
     path('register/', RegistrationView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),  # à décommenter lorsque la connexion avec JWT sera utilisée
     path('transactions/', AddTransactionView.as_view(), name='add_transaction'),
     path('transactions/<int:user_id>/', ListTransactionsView.as_view(), name='list_transactions'),
     path('budget/<int:user_id>/', BudgetView.as_view(), name='get_budget'),
